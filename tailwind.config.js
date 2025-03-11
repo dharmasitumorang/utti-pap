c587fcc3b807bf3bc9686b1d5a6bf2660e7a0d2b
Luhur65 <dharmabakti1202@gmail.com>
2025-03-11 16:08:52 +0700

    #1 Make Hero Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
       fontFamily: {
         title: ['DM Sans', 'sans-serif'],
         text: ['Playfair Display', 'serif'],
-      }
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'goyang': 'goyang 1s ease-in-out infinite'
+      },
+      keyframes: {
+        'goyang': {
+          '0%, 100%': {
+            transform: 'rotate(-3deg)'
+          },
+          '50%': {
+            transform: 'rotate(3deg)'
+          },
+        },
+      },
     },
   },
   plugins: [],
